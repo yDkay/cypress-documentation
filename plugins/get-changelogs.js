55a5e4dce2d3ff2333abb09a502a305421b1c89a
Paul Jaffre <jaffrepaul@gmail.com>
2022-09-23 17:21:15 -0400

    populate TOC hack list WIP

--- plugins/get-changelogs.js
+++ plugins/get-changelogs.js
@@ -5,25 +5,34 @@ module.exports = async function getChangelogs(context, options) {
     return {
       name: 'changelog-data',
       async loadContent() {
+        const changelogDir = path.resolve('docs/guides/references/_changelogs');
+        const changelogListFile = path.resolve('docs/guides/references/_changelog-list.mdx');
+
         // get changelog files & sort desc
-        const dirPath = path.resolve('docs/guides/references/_changelogs');
         try {
-          const changelogs = await fs.promises.readdir(dirPath);
+          const changelogs = await fs.promises.readdir(changelogDir);
           const sortedChangelogs = changelogs.sort((a, b) => {
             return b.localeCompare(a, undefined, { numeric: true, sensitivity: 'base' });
           })
-         // read each changlelog file to get contents & store for consumption
-         const data = []
-          for (const file of sortedChangelogs) {
-            const filePath = path.join(dirPath, file);
-            const contents = await fs.promises.readFile(filePath, { encoding: 'utf8' })
-            data.push(contents)
-          }
+
+          const data = []
+            for (const file of sortedChangelogs) {
+              // populate changelog list for TOC hack
+              // TODO: add logic to check file contents before writing
+              // const releaseNumHeader = `## ${file.slice(0, -3)}`+'\n';
+              // const toc = await fs.promises.appendFile(changelogListFile, releaseNumHeader)
+
+              // read each changlelog file to get contents & store for consumption
+              const changelogFile = path.join(changelogDir, file);
+              const contents = await fs.promises.readFile(changelogFile, { encoding: 'utf8' })
+              data.push(contents)
+            }
           return data;
         } catch (err) {
           console.error(err);
         }
       },
+      // pass sorted changelog data for storage & consumption by <Changelog />
       async contentLoaded({content, actions}) {
         const {setGlobalData} = actions;
         setGlobalData({changelogs: content});
